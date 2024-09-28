13c3067df81c478ae3859bc09259ed480e981706
Francisco Beccaria <fbeccaria24@gmail.com>
2024-09-28 10:45:49 -0300

    chore: configure remotePatterns for external images in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'grid0.vtexassets.com',
+        port: '',
+        pathname: '/arquivos/ids/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.adidas.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
