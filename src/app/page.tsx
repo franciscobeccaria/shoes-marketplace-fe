8f26b661273eb7c12c08d01f95a36e1b47be08a4
Francisco Beccaria <fbeccaria24@gmail.com>
2024-09-29 10:08:34 -0300

    feat: revalidate after 1 minute and scrape the data after revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { ProductPlatform } from "@/components/product-platform";
 
+export const revalidate = 60; // 86400 seconds is 1 day
+
 export default async function Home() {
+  await fetch('https://shoes-marketplace-backend-d76726f71a85.herokuapp.com/products/scrape/adidas')
+  await fetch('https://shoes-marketplace-backend-d76726f71a85.herokuapp.com/products/scrape/grid')
   const data = await fetch('https://shoes-marketplace-backend-d76726f71a85.herokuapp.com/products')
   const products = await data.json()
   return (
